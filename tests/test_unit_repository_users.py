02749d8fe2b837870564de8cc63e9a81a8c8f163
ZatokaV <zatokav@gmail.com>
2023-03-12 17:20:24 +0200

    Code refactoring

---
+++ tests/test_unit_repository_users.py
@@ -0,0 +1,38 @@
+import unittest
+from unittest.mock import MagicMock
+
+from sqlalchemy.orm import Session
+
+from src.database.models import User
+from src.repository.users import create_user, get_user_by_email
+from src.schemas import UserModel
+
+
+class TestUser(unittest.IsolatedAsyncioTestCase):
+
+    def setUp(self):
+        self.session = MagicMock(spec=Session)
+        self.body = UserModel(
+            username="Vitalii",
+            email="vitalii@email.com",
+            password="1234567890"
+        )
+
+    async def test_create_user(self):
+        body = self.body
+        result = await create_user(body=body, db=self.session)
+        self.assertEqual(result.username, body.username)
+        self.assertEqual(result.email, body.email)
+        self.assertEqual(result.password, body.password)
+        self.assertTrue(hasattr(result, "id"))
+
+    async def test_get_user_by_email(self):
+        body = self.body
+        await create_user(body=body, db=self.session)
+        user = self.session.query(User).filter(User.email == body.email).first().email
+        result = await get_user_by_email(email=body.email, db=self.session)
+        self.assertEqual(result.email, user)
+
+
+if __name__ == '__main__':
+    unittest.main()
